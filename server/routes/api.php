cbc3aff69c67ddefbe1ba8eff4df1bef66d1ebae
tyrkinn <nikemad61@gmail.com>
2022-10-27 18:00:18 +0300

    :art: Cleanup server dir

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
