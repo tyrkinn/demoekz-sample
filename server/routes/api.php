c1e4b6dc5b90fdefd73fa3d49ebd3e20eaa801a2
tyrkinn <nikemad61@gmail.com>
2022-10-27 18:08:19 +0300

    :bug: Fix controller bug

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,7 +2,6 @@
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
